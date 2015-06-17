2ae4ca06f689bce2335a671928fb02a1a8a5d67b
XuLiang <1597144301@qq.com>
2015-06-17 22:08:03 +0800

    Implement easy function.

--- src/com/xuliang/swiperefreshlayoutdemo/MainActivity.java
+++ src/com/xuliang/swiperefreshlayoutdemo/MainActivity.java
@@ -1,16 +1,69 @@
 package com.xuliang.swiperefreshlayoutdemo;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnRefreshListener{
+
+	private SwipeRefreshLayout mSwipeLayout;
+	private ListView mListView;
+	private ArrayList<String> mList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		init();
+	}
+
+	/**
+	 * 初始化控件和数据
+	 */
+	private void init() {
+		// TODO Auto-generated method stub
+		mListView = (ListView) findViewById(R.id.listview);
+		mListView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, getListData()));
+		
+		mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swiper_layout);
+		mSwipeLayout.setOnRefreshListener(this);
+		mSwipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+				android.R.color.holo_green_light, android.R.color.holo_orange_light,
+				android.R.color.holo_red_light);
+	}
+
+	private ArrayList<String> getListData() {
+		// TODO Auto-generated method stub
+		mList = new ArrayList<String>();
+		mList.add("Hello !");
+		mList.add("This is easy-money-sniper .");
+		mList.add("This is a SwipeRefreshLayoutDemo .");
+		mList.add("You can pull it down to refresh !");
+		mList.add("Easy to use !");
+		return mList;
+	}
+
+	@Override
+	public void onRefresh() {
+		// TODO Auto-generated method stub
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				mSwipeLayout.setRefreshing(false);
+			}
+		}, 5000);
 	}
 
 	@Override
@@ -31,4 +84,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
